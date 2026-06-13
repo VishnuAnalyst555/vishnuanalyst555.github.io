b642dbcfa7ed79d6aef54162aacee2b408c7d85d
VishnuAnalyst555 <vishnubr.nitt.me@gmail.com>
2026-06-13 01:09:40 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("Website Loaded");
+window.addEventListener("load",()=>{
+
+document.body.style.opacity="0";
+
+setTimeout(()=>{
+document.body.style.transition="opacity 1s ease";
+document.body.style.opacity="1";
+},100);
+
+});
